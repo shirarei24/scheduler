b76e8f2ce761787c799ada0f44e8df1181c4d4a9
takatoki <nagano@hi.is.kit.ac.jp>
2016-05-20 17:07:09 +0900

    時間軸，データベース

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
- config.i18n.default_locale = :ja
+ #config.i18n.default_locale = :ja
 # Initialize the Rails application.
 Rails.application.initialize!
