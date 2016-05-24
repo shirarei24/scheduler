9714da500b4060a60ebfbc450203cf9961166cda
Reiki <periophthalmus24@gmail.com>
2016-05-24 10:48:19 +0900

    マージ用

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523085344) do
+ActiveRecord::Schema.define(version: 20160524002032) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
